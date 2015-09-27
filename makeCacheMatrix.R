0b79ffd1f6015bb5c92cb1efee10977a7224d162
ticorunner <erick.amador2501@gmail.com>
2015-09-27 15:41:26 -0600

    Program Assignment 2

---
+++ makeCacheMatrix.R
@@ -0,0 +1,51 @@
+makeCacheMatrix <- function (matx = matrix()) {
+
+# Class variables
+  invmat <- NULL
+  
+# Class methods  
+  set <- function(maty) {
+    matx <<- maty
+    invmat <<- NULL
+  }
+  
+  get <- function() {
+    matx
+  }
+  
+# Specific methods to set/get class variables
+  setinverse <- function(inverse) {
+    invmat <<- inverse
+  }
+  
+  getinverse <- function() {
+    invmat
+  }
+  
+# Default declaration to present all methods
+  # I did not understand very well this declaration, but if I did not put it, i got an error:
+  # "Error in x$getinverse : object of type 'closure' is not subsettable", which could not resolve in another way
+  
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)	
+  
+}
+
+cacheSolve <- function(x, ...) {
+
+  # Getting current inverse matrix state for the matrix x passed as argument
+  invmat <- x$getinverse()
+
+  # Determine if the inverse matrix has been calculated
+  if(!is.null(invmat)) {
+    message("Reading from cache")
+    return(invmat)
+  }
+  
+  data <- x$get()
+  invmat <- solve(data, ...)
+  x$setinverse(invmat)
+  
+  invmat    
+}
